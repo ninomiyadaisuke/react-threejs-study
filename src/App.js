31ad373ad7f523718ceb207581a2d6647f9e1b22
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2025-01-10 18:12:58 +0900

    vercel deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Images } from "./components";
 import { Canvas } from "@react-three/fiber";
+import { Scroll, ScrollControls } from "@react-three/drei";
 
 const images = [
   { url: "./img1.jpg", scale: [4, 3, 1], position: [-1, 0, 1] },
@@ -12,7 +13,15 @@ const images = [
 function App() {
   return (
     <Canvas>
-      <Images images={images} />
+      <ScrollControls pages={2} damping={2}>
+        <Scroll>
+          <Images images={images} />
+        </Scroll>
+        <Scroll html>
+          <h1 style={{ position: "absolute", top: "60vh", left: "1.5em" }}>Be</h1>
+          <h1 style={{ position: "absolute", top: "140vh", left: "40vw" }}>Creative</h1>
+        </Scroll>
+      </ScrollControls>
     </Canvas>
   );
 }
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import { Image } from '@react-three/drei'
+import React, { useRef } from 'react'
+import { Image, useScroll } from '@react-three/drei'
+import { useFrame, useThree } from '@react-three/fiber'
 
 const Images = (props) => {
   const { images } = props
+  const { width, height } = useThree((state) => state.viewport)
+  const group = useRef()
+  const data = useScroll()
+
+  useFrame(() => {
+    group.current.children[0].material.zoom = 1 + data.range(0, 1 / 3) / 3
+    group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3
+    group.current.children[2].material.zoom = 1 + data.range(0, 1 / 3) / 3
+    group.current.children[3].material.zoom = 1 + data.range(0, 1 / 3) / 3
+  })
+
   return (
-    <group>
-      {images.map((image) => <Image key={image.url} url={image.url} scale={image.scale} position={image.position} />)}
+    <group ref={group}>
+      <Image url="./img1.jpg" scale={[4, height, 1]} position={[-1, 0, 1]} />
+      <Image url="./img2.jpg" scale={3} position={[2, 0, 1]} />
+      <Image
+        url="./img3.jpg"
+        scale={[1, 3.5, 1]}
+        position={[-2.3, -height, 2]}
+      />
+      <Image
+        url="./img4.jpg"
+        scale={[1.4, 2, 1]}
+        position={[1.3, -height - 0.3, 3.2]}
+      />
     </group>
-    // <group><Image url='./img1.jpg' scale={[4, 3, 3]} position={[-1, 0, 1]} /></group>
+    // <group>
+    //   {images.map((image) => <Image key={image.url} url={image.url} scale={image.scale} position={image.position} />)}
+    // </group>
   )
 }
 
